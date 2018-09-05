7143eccbd543117ac560276d62fec2e363d71113
Anastasiia <afesenk@gmail.com>
2018-09-05 22:39:31 +0300

    Remove ui-bootstrap

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,11 +1,10 @@
 (function () {
     'use strict';
 
-    var app = angular.module('D3-app', ['d3','ngAnimate', 'ui.bootstrap']);
+    var app = angular.module('D3-app', ['d3','ngAnimate']);
 
     app.service('DrawMatrix', DrawMatrix);
     app.service('Zoom', Zoom);
     app.directive('seatMap', ['d3Service', 'DrawMatrix', 'Zoom', seatMap]);
-    app.controller('popoverCtrl', popoverCtrl);
 
 })();
